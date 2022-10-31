961f7007ede588ae1fa4f4f8e1a0e749b0c73abd
Aaron Kim <96655366+youknow-yoonho@users.noreply.github.com>
2022-10-31 14:50:46 +0900

    Get Country Code

--- app/src/main/java/net/spooncast/ext/sample/MainActivity.kt
+++ app/src/main/java/net/spooncast/ext/sample/MainActivity.kt
@@ -2,10 +2,15 @@ package net.spooncast.ext.sample
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import net.spooncast.ext.context.getCountryCode
+import net.spooncast.ext.context.toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        toast(getCountryCode())
     }
 }
