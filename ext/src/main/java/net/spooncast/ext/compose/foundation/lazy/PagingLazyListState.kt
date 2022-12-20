9c2ac2a0e136d0c2fc95e4734a69ea680d2e2153
Yoonho Aaron Kim <96655366+youknow-yoonho@users.noreply.github.com>
2022-12-20 19:00:44 +0900

    rememberPagingLazyListState 추가

---
+++ ext/src/main/java/net/spooncast/ext/compose/foundation/lazy/PagingLazyListState.kt
@@ -0,0 +1,31 @@
+package net.spooncast.ext.compose.foundation.lazy
+
+import androidx.compose.foundation.lazy.LazyListState
+import androidx.compose.foundation.lazy.rememberLazyListState
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.derivedStateOf
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
+
+@Composable
+fun rememberPagingLazyListState(
+    prefetchDistance: Int = 5,
+    onScrollToBottom: () -> Unit
+): LazyListState {
+    val listState = rememberLazyListState()
+    val shouldLoadMore by remember {
+        derivedStateOf {
+            val lastVisibleItemIdx = listState.layoutInfo.visibleItemsInfo.lastOrNull()?.index ?: Int.MIN_VALUE
+            val lastIndex = listState.layoutInfo.totalItemsCount - 1
+            lastVisibleItemIdx >= lastIndex - prefetchDistance
+        }
+    }
+    LaunchedEffect(key1 = shouldLoadMore) {
+        if (shouldLoadMore) {
+            onScrollToBottom()
+        }
+    }
+
+    return listState
+}
