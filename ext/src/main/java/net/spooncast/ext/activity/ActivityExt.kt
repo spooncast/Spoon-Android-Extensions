de20eac11950f412cd7865793958cb2ceaac9806
Yoonho Aaron Kim <96655366+youknow-yoonho@users.noreply.github.com>
2023-07-04 14:40:20 +0900

    Update ActivityExt.kt

--- ext/src/main/java/net/spooncast/ext/activity/ActivityExt.kt
+++ ext/src/main/java/net/spooncast/ext/activity/ActivityExt.kt
@@ -6,6 +6,9 @@ import android.view.View
 import android.view.inputmethod.InputMethodManager
 import android.widget.EditText
 
+val Activity?.isFinishingOrDestroyed: Boolean
+    get() = this == null || this.isFinishing || this.isDestroyed
+
 private val Activity?.inputMethodManager: InputMethodManager?
     get() = this?.getSystemService(Context.INPUT_METHOD_SERVICE) as? InputMethodManager
 
