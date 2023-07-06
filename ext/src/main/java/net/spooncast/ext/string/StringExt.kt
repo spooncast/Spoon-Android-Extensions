7d977dc54c426ab9add90c1c882171c3436ce4cc
Yoonho Aaron Kim <96655366+youknow-yoonho@users.noreply.github.com>
2023-07-07 08:32:15 +0900

    Add removeNewLine function

--- ext/src/main/java/net/spooncast/ext/string/StringExt.kt
+++ ext/src/main/java/net/spooncast/ext/string/StringExt.kt
@@ -3,3 +3,6 @@ package net.spooncast.ext.string
 fun String.substringByLength(length: Int): String {
     return this.substring(0 until length.coerceAtMost(this.length))
 }
+
+val CharSequence?.removeNewLine : CharSequence
+    get() = this?.replace("(\r\n|\r|\n|\n\r)".toRegex(), "") ?: ""
