50135a1c67684a8d76448f6645be4614fef04e0a
SeopSeo <seop@spoonradio.co>
2023-08-11 19:16:26 +0900

    StringInputFilter 추가

---
+++ ext/src/main/java/net/spooncast/ext/string/StringInputFilter.kt
@@ -0,0 +1,23 @@
+package net.spooncast.ext.string
+
+import android.text.InputFilter
+import android.text.Spanned
+
+class StringInputFilter(
+    val regex: Regex
+): InputFilter {
+    override fun filter(
+        source: CharSequence,
+        start: Int,
+        end: Int,
+        dest: Spanned?,
+        dstart: Int,
+        dend: Int
+    ): CharSequence {
+        return if (source.matches(regex)) {
+            source
+        } else {
+            ""
+        }
+    }
+}
