fe47ab0765b3e9bac122f7d752300796f2fa2d61
Yoonho Aaron Kim <96655366+youknow-yoonho@users.noreply.github.com>
2023-11-02 18:05:54 +0900

    Update UriExt.kt

--- ext/src/main/java/net/spooncast/ext/uri/UriExt.kt
+++ ext/src/main/java/net/spooncast/ext/uri/UriExt.kt
@@ -2,14 +2,23 @@ package net.spooncast.ext.uri
 
 import android.content.ContentResolver
 import android.graphics.Bitmap
+import android.graphics.BitmapFactory
 import android.graphics.ImageDecoder
 import android.net.Uri
 import android.os.Build
+import android.webkit.URLUtil
 import androidx.annotation.RequiresApi
+import java.net.URL
+
 
 @RequiresApi(Build.VERSION_CODES.P)
 fun Uri.toBitmap(contentResolver: ContentResolver): Bitmap? {
     return runCatching {
-        ImageDecoder.decodeBitmap(ImageDecoder.createSource(contentResolver, this))
+        if (URLUtil.isNetworkUrl(this.toString())) {
+            val url = URL(this.toString())
+            BitmapFactory.decodeStream(url.openConnection().getInputStream())
+        } else {
+            ImageDecoder.decodeBitmap(ImageDecoder.createSource(contentResolver, this))
+        }
     }.getOrNull()
 }
