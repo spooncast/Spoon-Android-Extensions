27cce31b05c339ec4de1646873373c3db7e1582e
Aaron <96655366+youknow-yoonho@users.noreply.github.com>
2024-07-23 10:01:01 +0900

    AOSTEAM-1896 [Ext] FixedStack 구현 (#23)
    * AOSTEAM-1896 [Ext] FixedStack 구현
    
    * dev
    
    ---------
    
    Co-authored-by: Yoonho Aaron Kim <youknow.yoonho@gmail.com>

---
+++ ext/src/main/java/net/spooncast/ext/common/collections/FixedStack.kt
@@ -0,0 +1,49 @@
+package net.spooncast.ext.common.collections
+
+import android.util.Log
+import java.util.LinkedList
+
+/**
+ * @property maxSize Stack의 최대 크기
+ * @property shiftWhenOverflow Stack이 가득찬 상태에서 추가 add가 들어올 때 shiftWhenOverflow 여부에 따라 오래된 값을 밀어내고 채워나갈지 결정
+ * @param initialValues Stack의 초기 데이터
+ */
+class FixedStack<T>(
+    private val maxSize: Int,
+    private val shiftWhenOverflow: Boolean = false,
+    initialValues: List<T> = emptyList()
+) {
+
+    private val data: LinkedList<T> = LinkedList<T>()
+
+    init {
+        if (initialValues.isNotEmpty()) {
+            data.addAll(initialValues.take(maxSize).reversed())
+        }
+    }
+
+    @Synchronized
+    fun add(element: T): Boolean {
+        return when (data.size < maxSize) {
+            true -> {
+                data.addFirst(element)
+                true
+            }
+
+            else -> {
+                if (shiftWhenOverflow) {
+                    data.removeLast()
+                    data.addFirst(element)
+                    true
+                } else {
+                    Log.w("FixedQueue", "[spoon][FixedQueue] add - failed: size over the maxSize($maxSize)")
+                    false
+                }
+            }
+        }
+    }
+
+    @Synchronized
+    operator fun get(index: Int = 0): T? = data[index]
+
+}
---
+++ ext/src/test/java/net/spooncast/ext/common/collections/FixedStackTest.kt
@@ -0,0 +1,56 @@
+package net.spooncast.ext.common.collections
+
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+
+class FixedStackTest {
+
+    @Test
+    fun constructTest() {
+        val stack = FixedStack(3, initialValues = listOf(1, 2, 3))
+
+        assertEquals(3, stack[0])
+        assertEquals(2, stack[1])
+        assertEquals(1, stack[2])
+    }
+
+    @Test
+    fun `addAndGetTest - no shift`() {
+        val stack = FixedStack<Int>(3)
+
+        stack.add(1)
+        stack.add(2)
+        assertEquals(2, stack[0])
+        assertEquals(1, stack[1])
+
+        stack.add(3)
+        assertEquals(3, stack[0])
+        assertEquals(2, stack[1])
+        assertEquals(1, stack[2])
+
+        stack.add(4)
+        assertEquals(3, stack[0])
+        assertEquals(2, stack[1])
+        assertEquals(1, stack[2])
+    }
+
+    @Test
+    fun `addAndGetTest - shift`() {
+        val stack = FixedStack<Int>(3, shiftWhenOverflow = true)
+
+        stack.add(1)
+        stack.add(2)
+        assertEquals(2, stack[0])
+        assertEquals(1, stack[1])
+
+        stack.add(3)
+        assertEquals(3, stack[0])
+        assertEquals(2, stack[1])
+        assertEquals(1, stack[2])
+
+        stack.add(4)
+        assertEquals(4, stack[0])
+        assertEquals(3, stack[1])
+        assertEquals(2, stack[2])
+    }
+}
